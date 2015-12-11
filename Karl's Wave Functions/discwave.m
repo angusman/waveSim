d4800d4ac08c38c6fbb62425800ba55d5a036da7
angusman <agusman@csu.fullerton.edu>
2015-12-10 21:12:45 -0800

    boundary conditions added

--- Karl's Wave Functions/discwave.m
+++ Karl's Wave Functions/discwave.m
@@ -1,14 +1,14 @@
-function [ x, tn, y ] = discwave( a, b, N, t0, tf, dt, v, f0, g0 )
+function [ x, tn, y ] = discwave( a, b, N, t0, tf, dt, v, f0, g0, bc )
 %DISCWAVE Solve wave equation and simulate continuous time solutions.
 %   
 
 dx = (b - a)/N;
 x = zeros(N + 2, 1);
-y = zeros(N + 2);
 t = t0:dt:tf;
 tn = length(t);
 y = zeros(N + 2, tn);
 c = (v^2 * dt^2)/(dx^2);
+
 
 for k = 1:(N + 2)
     x(k) = a + k * dx;
@@ -17,16 +17,32 @@ for k = 1:(N + 2)
 end
 
 for n = 3:tn
-    for k = 2:(N - 1)
+    for k = 2:(N + 1)
         yknm = 2 * y(k, n - 1);
         y(k, n) = yknm - y(k, n - 2) + ...
             c * (y(k + 1, n - 1) - yknm + y(k - 1, n - 1));
     end
+    
+    switch bc
+        case 'dir'
+            y(1, n) = 0;
+            y(N + 2, n) = 0;
+        case 'neu'
+            y(1, n) = y(2, n);
+            y(N + 2, n) = y(N + 1, n);
+        case 'nd'
+            y(1, n) = y(2, n);
+            y(N + 2, n) = 0;
+        case 'dn'
+            y(1, n) = 0;
+            y(N + 2, n) = y(N + 1, n);
+    end
 end
+
 
 figure;
 for n = 1:tn
-    plot(x, y(:, n));
+    plot(x, y(:, n), 'LineWidth', 4);
     axis([-20 20 -1 1]);
     M(n) = getframe(gcf);
 end
@@ -34,4 +50,10 @@ end
 ntimes = 3;
 fps = 1;
 movie(gcf, M, ntimes, fps);
+
+myVideo = VideoWriter('q2.avi');
+open(myVideo);
+writeVideo(myVideo, M);
+close(myVideo);
+
 end
--- Karl's Wave Functions/wavescript.m
+++ Karl's Wave Functions/wavescript.m
@@ -3,9 +3,10 @@ f1 = @(x) sin(x);
 g0 = @(x) 0;
 g1 = @(x) cos(x);
 ff = @(x) f2(x);
+fff = @(a, b, k) (@(x) sin(k * pi * (x - a)/(b - a)));
 
 f3 = @(x) exp(-x.^2);
 g3 = @(x) 2 * x .* exp(-x.^2);
 
-discwave(-20, 20, 1000, 0, 5, 0.01, 1, f3, g3);
+discwave(0, 10, 100, 0, 20, 0.1, 1, f0, g0, 'dir');
 
