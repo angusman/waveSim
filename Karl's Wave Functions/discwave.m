66c749c13c427932c2b8911e92efd9b97d1af07e
Nicholas Sullivan <nick@Nicholass-MacBook-Pro.local>
2015-12-10 21:24:59 -0800

    adding comments for readability

--- Karl's Wave Functions/discwave.m
+++ Karl's Wave Functions/discwave.m
@@ -2,6 +2,7 @@ function [ x, tn, y ] = discwave( a, b, N, t0, tf, dt, v, f0, g0, bc )
 %DISCWAVE Solve wave equation and simulate continuous time solutions.
 %   
 
+%converting params to intital values
 dx = (b - a)/N;
 x = zeros(N + 2, 1);
 t = t0:dt:tf;
@@ -10,12 +11,14 @@ y = zeros(N + 2, tn);
 c = (v^2 * dt^2)/(dx^2);
 
 
+%setting the intial conditions of the wave
 for k = 1:(N + 2)
     x(k) = a + k * dx;
     y(k, 1) = f0(x(k)) - dt * g0(x(k));
     y(k, 2) = f0(x(k));
 end
 
+%running the formula to produce the wave simulation
 for n = 3:tn
     for k = 2:(N + 1)
         yknm = 2 * y(k, n - 1);
@@ -23,6 +26,7 @@ for n = 3:tn
             c * (y(k + 1, n - 1) - yknm + y(k - 1, n - 1));
     end
     
+    %switiching over boundary conditions
     switch bc
         case 'dir'
             y(1, n) = 0;
@@ -40,6 +44,7 @@ for n = 3:tn
 end
 
 
+$ploting and such
 figure;
 for n = 1:tn
     plot(x, y(:, n), 'LineWidth', 4);
@@ -47,6 +52,7 @@ for n = 1:tn
     M(n) = getframe(gcf);
 end
 
+%producing a .avi movie is results
 ntimes = 3;
 fps = 1;
 movie(gcf, M, ntimes, fps);
